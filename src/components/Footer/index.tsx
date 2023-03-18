e35aa698799cf2801f6bb010a182295a7243def7
Jhonatanmizu <natanjesuss20@gmail.com>
2023-03-18 20:43:01 -0300

    :sparkles: created footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './styles';
+const Footer: React.FC = () => {
+  return <S.FooterWrapper>Footer</S.FooterWrapper>;
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.footer`
+  width: 100%;
+  height: 6.4rem;
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
