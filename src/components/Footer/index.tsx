bc1832bb5042129e94af7209aa118732d79e9853
Jhonatanmizu <natanjesuss20@gmail.com>
2023-03-18 22:40:53 -0300

    :recycle: finished footer layout

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import * as S from './styles';
+import { MdFavorite } from 'react-icons/md';
 const Footer: React.FC = () => {
-  return <S.FooterWrapper>Footer</S.FooterWrapper>;
+  return (
+    <S.FooterWrapper>
+      Made With{' '}
+      <S.HeartWrapper>
+        <MdFavorite size={30} />
+      </S.HeartWrapper>
+      By <S.FooterLink>Jhonatan Jesus</S.FooterLink>
+    </S.FooterWrapper>
+  );
 };
 
 export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,4 +1,10 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+export const floatingAnimation = keyframes`
+   0% { transform:translateY(0px);}
+ 50% { transform:translateY(-15px);}
+ 100% { transform:translateY(0px); }
+`;
 
 export const FooterWrapper = styled.footer`
   width: 100%;
@@ -6,5 +12,23 @@ export const FooterWrapper = styled.footer`
   background-color: ${({ theme }) => theme.palette.primary.main};
   display: flex;
   align-items: center;
+  gap: 0.5rem;
   justify-content: center;
+`;
+
+export const FooterLink = styled.a`
+  color: ${({ theme }) => theme.palette.background.main};
+  cursor: pointer;
+  transition: all 0.5s ease-in-out;
+  :hover {
+    opacity: 0.6;
+  }
+  font-weight: 700;
+`;
+
+export const HeartWrapper = styled.div`
+  transform: translateY(0px);
+  animation-name: ${floatingAnimation};
+  animation-duration: 5s;
+  animation-iteration-count: infinite;
 `;
